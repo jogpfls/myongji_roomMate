c7364bd1c4e7c4c5ee382676a111a98f5f5553f6
Heesu01 <rulru0413@naver.com>
2024-08-11 14:15:33 +0900

    feat: 카테고리 컴포넌트 UI 구현

---
+++ src/components/Category.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Category = ({ children, onClick, bgc }) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleClick = () => {
+    setIsSelected(!isSelected);
+    if (onClick) onClick();
+  };
+
+  return (
+    <div>
+      <CaBox onClick={handleClick} bgc={bgc} isSelected={isSelected}>
+        # {children}
+      </CaBox>
+    </div>
+  );
+};
+
+const CaBox = styled.div`
+  width: 165px;
+  height: 48px;
+  border-radius: 30px;
+  background-color: ${(props) =>
+    props.isSelected
+      ? props.bgc || props.theme.colors.blue2
+      : props.theme.colors.lightBlue};
+  color: ${(props) => (props.isSelected ? "white" : "black")};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+  font-size: 20px;
+`;
+
+export default Category;
