d026723fd12ca148f41180e193e6d633850ef665
jogpfls <joerin4177@gmail.com>
2024-08-15 07:44:45 +0900

    feat: 반응형 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useLocation, useNavigate } from "react-router-dom";
-import { FaHome, FaBuilding } from "react-icons/fa";
+import { FaHome, FaBuilding, FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const activePath = location.pathname.toLowerCase();
 
   const isActive = (path) => {
@@ -16,18 +18,22 @@ const Header = () => {
 
   const handleNavigation = (path) => {
     navigate(path);
+    setMenuOpen(false);
   };
 
   const handleMouseEnter = () => {
+    if (window.innerWidth > 480)
     setIsOpen(true);
   };
 
   const handleMouseLeave = () => {
+    if (window.innerWidth > 480)
     setIsOpen(false);
   };
 
   useEffect(() => {
     const onClick = (e) => {
+      setWindowWidth(window.innerWidth);
       if (menuRef.current !== null && !menuRef.current.contains(e.target)) {
         setIsOpen(false);
       }
@@ -40,10 +46,33 @@ const Header = () => {
     };
   }, [isOpen]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Container>
-      <Title onClick={() => handleNavigation("/main")}>명지메이트</Title>
-      <Menu>
+      <Box>
+      <Sidebar>
+        <Title onClick={() => handleNavigation("/main")}>명지메이트</Title>
+        {windowWidth <=480 &&
+          <LogoutContainer>
+            <Logout onClick={() => navigate("/auth/login")}>로그인</Logout>
+          </LogoutContainer>
+        }
+        <SideMenu onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <Close /> : <MenuBars />}
+        </SideMenu>
+      </Sidebar>
+      <Menu open={menuOpen}>
       <Item
           $active={isActive("/info")}
           onClick={() => handleNavigation("/info")}
@@ -55,14 +84,25 @@ const Header = () => {
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
+          {windowWidth > 480 ? 
           <Item
-            $active={
-              isActive("/dormitory") || isActive("/room") || isActive("/write")
-            }
-            post="매칭"
+          $active={
+            isActive("/dormitory") || isActive("/room") || isActive("/write")
+          }
+          post="매칭"
           >
-            매칭게시판
-          </Item>
+          매칭게시판
+          </Item> : 
+          <Item
+          $active={
+            isActive("/main")
+          }
+          onClick={() => handleNavigation("/main")}
+        >
+          매칭게시판
+        </Item>  
+        }
+          
           {isOpen && (
             <DroppedBox>
               <LinkWrapper onClick={() => handleNavigation("/dormitory")}>
@@ -95,10 +135,13 @@ const Header = () => {
         >
           마이페이지
         </Item>
-        <LogoutContainer>
-        <Logout onClick={() => navigate("/auth/login")}>로그인</Logout>
-      </LogoutContainer>
+        {windowWidth > 480 &&
+          <LogoutContainer>
+            <Logout onClick={() => navigate("/auth/login")}>로그인</Logout>
+          </LogoutContainer>
+        }
       </Menu>
+      </Box>
     </Container>
   );
 };
@@ -110,19 +153,65 @@ const Container = styled.div`
   height: 60px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
   border-bottom: 1px solid ${(props) => props.theme.colors.gray};
   background-color: ${(props) => props.theme.colors.white};
-  padding-top: 10px;
+
+  @media screen and (max-width: ${({theme})=>theme.breakpoints.mobile}){
+    padding: 10px;
+    flex-direction: column;
+    padding: 0;
+  }
+`;
+
+const Box = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 3vw;
+`;
+
+const Sidebar = styled.div`
+  display: flex;
+  align-items: center;
+  width: 30%;
+  justify-content: space-between;
+
+  @media (max-width: ${({theme})=>theme.breakpoints.mobile}) {
+    width: 100%;
+  }
+`;
+
+const SideMenu = styled.div`
+  display: none;
+  font-size: 24px;
+  cursor: pointer;
+
+  @media (max-width: ${({theme})=>theme.breakpoints.mobile}) {
+    display: block;
+    margin-right: 3vw;
+  }
+`;
+
+const MenuBars = styled(FaBars)`
+  color: ${(props) => props.theme.colors.deepBlue};
+`;
+
+const Close = styled(FaTimes)`
+  color: ${(props) => props.theme.colors.deepBlue};
 `;
 
 const Title = styled.p`
-  width: 33%;
+  width: 100%;
   ${(props) => props.theme.fonts.logo};
-  padding-left: 3vw;
   cursor: pointer;
   color: ${(props) => props.theme.colors.deepBlue};
-  white-space: nowrap
+  white-space: nowrap;
+
+  @media screen and (max-width: ${({theme})=>theme.breakpoints.mobile}){
+    font-size: 5.5vw;
+    margin-left: 3vw;
+  }
 `;
 
 const Menu = styled.h1`
@@ -131,19 +220,32 @@ const Menu = styled.h1`
   align-items: center;
   justify-content: space-between;
   font-size: 20px;
-  margin-right: 7vw;
+
+  @media (max-width: ${({theme})=>theme.breakpoints.mobile}) {
+    width: 100%;
+    flex-direction: column;
+    gap: 5px;
+    display: ${(props) => (props.open ? "flex" : "none")};
+    background-color: ${({theme})=>theme.colors.white};
+    position: absolute;
+    top: 60px;
+    left: 0;
+    z-index: 999;
+  }
+
 `;
 
 const ItemBox = styled.div`
   position: relative;
-  display: inline-block;
+  display: flex;
+  flex-direction: columns;
 `;
 
 const Item = styled.span`
   margin-left: 3vw;
   text-align: center;
   white-space: nowrap;
-  padding: 15px 0;
+  padding: 20px 0;
   font-weight: ${(props) => (props.$active ? "bold" : "normal")};
   border-bottom: ${(props) =>
     props.$active ? `4px solid ${props.theme.colors.deepBlue}` : "none"};
@@ -164,6 +266,19 @@ const Item = styled.span`
     &:hover {
       color: ${theme.colors.deepBlue};
     }`}
+
+@media (max-width: ${({theme})=>theme.breakpoints.mobile}) {
+    font-size: 18px;
+    width: 100vw;
+    border-bottom: solid 2px ${({theme})=>theme.colors.gray2};
+    padding: 20px 0;
+    margin: 0;
+
+    &:hover{
+      color: ${({theme})=>theme.colors.deepBlue};
+      border-bottom: solid 2px ${({theme})=>theme.colors.gray2
+    }
+  }}
 `;
 
 const DroppedBox = styled.div`
@@ -183,6 +298,15 @@ const DroppedBox = styled.div`
   opacity: 0.95;
   transform-origin: top;
   transform: scaleY(1);
+
+  @media (max-width: ${({theme})=>theme.breakpoints.mobile}) {
+    position: relative;
+    top: 0;
+    left: 0;
+    box-shadow: none;
+    width: 100%;
+  }
+
 `;
 
 const LinkWrapper = styled.a`
@@ -201,25 +325,41 @@ const LinkWrapper = styled.a`
     background-color: ${({ theme }) => theme.colors.deepBlue};
     color: ${({ theme }) => theme.colors.white};
   }
+
+  @media (max-width: ${({theme})=>theme.breakpoints.mobile}) {
+    justify-content: flex-start;
+    padding: 10px;
+  }
 `;
 
 const LogoutContainer = styled.div`
   margin-left: 3vw;
+  height: 100%;
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: ${({theme})=>theme.breakpoints.mobile}){
+    margin-left: 38vw;
+    margin-right: 3vw;
+  }
 `;
 
 const Logout = styled.button`
   width: 8vw;
   min-width: 50px;
-  height: 4vh;
-  border-radius: 2vw;
+  height: 35px;
+  border-radius: 15px;
   background-color: ${(props) => props.theme.colors.deepBlue};
   color: ${(props) => props.theme.colors.white};
-  margin-bottom: 3px;
   cursor: pointer;
   transition: background-color 0.3s ease;
 
   &:hover {
     background-color: ${(props) => props.theme.colors.blueBlack};
+  }
+
+  @media (max-width: ${({theme})=>theme.breakpoints.mobile}) {
+    width: 100%;
   }
 `;
 
