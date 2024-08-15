ab3d1ada40a94ea3848310d540af9428420cc42c
Heesu01 <rulru0413@naver.com>
2024-08-15 19:45:41 +0900

    feat: 로그인api 연결

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || "https://api.mutsideout.com",
+  baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: process.env.REACT_APP_BASE_URL || "https://localhost:8080",
   withCredentials: true,
 });
