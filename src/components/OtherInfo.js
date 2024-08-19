b206095cbe42e5a171f307e38dcde2466b8f89c2
Heesu01 <rulru0413@naver.com>
2024-08-20 01:50:18 +0900

    feat: 반응형 추가 + 디자인 수정

--- src/components/OtherInfo.js
+++ src/components/OtherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Category from "./Category";
+import tag from "../images/tag.png";
 
 const OtherInfo = ({ userInfo, onClose }) => {
   return (
@@ -8,6 +9,7 @@ const OtherInfo = ({ userInfo, onClose }) => {
       <ModalContent>
         <Header>
           <h2>{userInfo.name}님의 INFO</h2>
+          <img src={tag}></img>
         </Header>
         <CategoryList>
           <CategoryWrapper>
@@ -42,60 +44,77 @@ const ModalOverlay = styled.div`
 const ModalContent = styled.div`
   background: #fff;
   border-radius: 12px;
-  padding: 30px;
-  max-width: 500px;
+  padding: 25px;
+  max-width: 550px;
   width: 60%;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  max-height: 80%;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
-    width: 70%;
+    width: 80%;
+    max-height: 60%;
   }
 `;
 
 const Header = styled.div`
-  margin-bottom: 15px;
+  margin-top: 10px;
   text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
 
   h2 {
     ${(props) => props.theme.fonts.text4};
   }
+
+  img {
+    width: 10%;
+  }
 `;
 
 const ButtonContainer = styled.div`
-  margin-top: 50px;
+  margin-top: 20px;
   display: flex;
-  justify-content: center;
+  justify-content: end;
   width: 100%;
 `;
 
 const ConfirmButton = styled.button`
-  background: ${(props) => props.theme.colors.blue};
+  background: ${(props) => props.theme.colors.deepBlue2};
   color: #fff;
   border: none;
   border-radius: 8px;
   padding: 10px 30px;
+  margin-right: 20px;
   cursor: pointer;
   transition: background-color 0.3s;
-
   &:hover {
-    background: ${(props) => props.theme.colors.deepBlue2};
+    background: ${(props) => props.theme.colors.deepBlue};
   }
 `;
 
 const CategoryList = styled.div`
   margin-top: 20px;
   display: flex;
-  align-items: center;
   justify-content: center;
-  width: 80%;
+  width: 90%;
+  border: 2px dashed ${(props) => props.theme.colors.deepBlue};
+  border-radius: 10px;
+  padding: 40px 20px;
+  overflow-y: auto;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    padding: 30px 10px;
+  }
 `;
 
 const CategoryWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
-  justify-content: flex-start;
+  justify-content: center;
 `;
