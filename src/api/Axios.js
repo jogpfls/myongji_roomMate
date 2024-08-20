63fc515ed98c045c629bf11bcfaa1112b84386e7
Heesu01 <rulru0413@naver.com>
2024-08-20 11:42:26 +0900

    refactor: 주석제거

--- src/api/Axios.js
+++ src/api/Axios.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const Axios = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-  // baseURL: process.env.REACT_APP_BASE_URL || "https://localhost:8080",
   withCredentials: true,
 });
